b1db71337e3ab778f6828be6e49ca39442a90a8f
Cory McCartan <cory.mccartan@gmail.com>
2019-12-30 00:40:17 -0800

    rebuilding site Mon Dec 30 00:40:17 PST 2019

--- js/unions-2019.js
+++ js/unions-2019.js
@@ -7,16 +7,35 @@ async function main() {
     await load_data();
 
     let intl_container = d3.select("#intl-chart");
+    window.w = d3.select("main").node().getBoundingClientRect().width;
+    window.h = innerHeight - d3.select("header").node().getBoundingClientRect().height;
+
     //makeIntlBubbles(intl_container, "net_assets", 12, 16);
-    makeIntlBubbles(intl_container);
+    window.intl_chart = makeIntlBubbles(intl_container);
+
+    let controller = new ScrollMagic.Controller();
+    let scene_1 = new ScrollMagic.Scene({
+        triggerElement: intl_container.node(),
+        triggerHook: 1,
+        offset: h,
+        duration: "200%",
+    })
+    .setPin(intl_container.node(), { pushFollowers: false })
+    .addTo(controller);
+    let scene_2 = new ScrollMagic.Scene({
+        triggerElement: "#a1s1t2",
+        triggerHook: 0.5,
+        //offset: h,
+    })
+    .on("enter", () => intl_chart.rescale("net_assets", 5, 15))
+    .addTo(controller);
+
 }
 
 window.palette = ["#5fb0e6", "#73bc5e", "#eebc3b", "#8c54b0", "#4c6060"];
 
-function makeIntlBubbles(container, radius="members", r_scale=20, r_div=12) {
+function makeIntlBubbles(container, radius="members", r_scale=10, r_div=11) {
     let svg = container.append("svg");
-    let w = container.node().getBoundingClientRect().width;
-    let h = 60 + w * 0.8;
     svg.attr("width", w)
         .attr("height", h);
 
@@ -26,21 +45,18 @@ function makeIntlBubbles(container, radius="members", r_scale=20, r_div=12) {
         .range([4, r_scale + w/r_div]);
     let colors = {"AFL-CIO": palette[0], "CTW": palette[1], "INDEP": palette[2]};
     let centers = {
-        "AFL-CIO": [w*0.4, h*0.4],
-        "CTW": [w*0.6, h*0.5],
-        "INDEP": [w*0.5, h*0.6],
+        "AFL-CIO": [w*0.3, h*0.2],
+        "CTW": [w*0.5, h*0.8],
+        "INDEP": [w*0.6, h*0.5],
     };
 
     let data = intls.filter(d => d.members > 50)
-    .map(d => ({
-        members: d.members,
-        net_assets: d.net_assets,
-        radius: radiusScale(d[radius]),
-        color: colors[d.affl],
-        label: d.abbr,
-        cx: centers[d.affl][0],
-        cy: centers[d.affl][1],
-        tooltip: d.union_name
+    .map(d => {
+        d.radius= radiusScale(d[radius]);
+        d.color= colors[d.affl];
+        d.cx = centers[d.affl][0];
+        d.cy = centers[d.affl][1];
+        d.tooltip = d.union_name
             .replace(", AFL-CIO", "")
             .replace(", AFLCIO", "")
             .replace(" AFL-CIO", "")
@@ -50,8 +66,9 @@ function makeIntlBubbles(container, radius="members", r_scale=20, r_div=12) {
             .replace(" WKRS", " WORKERS")
             .replace(" EMPLS", " EMPLOYEES")
             .replace(" ASN", " ASSOCIATION")
-            .replace(" DEPT", " DEPARTMENT")
-    }));
+            .replace(" DEPT", " DEPARTMENT");
+        return d;
+    });
 
     let nodes = svg.selectAll("g.intl")
         .data(data)
@@ -63,10 +80,10 @@ function makeIntlBubbles(container, radius="members", r_scale=20, r_div=12) {
         .attr("fill", d => d.color)
 
     let labels = nodes.append("text")
-        .text(d => d.radius > 10 ? d.label : "")
+        .text(d => d.radius > 10 ? d.abbr : "")
         .attr("text-anchor", "middle")
         .attr("alignment-baseline", "middle")
-        .attr("font-size", d => 3*d.radius / (d.label.length+2))
+        .attr("font-size", d => 3*d.radius / (d.abbr.length+2))
         .attr("font-weight", "bold")
         .attr("fill", "white")
         .attr("x", 0)
@@ -77,15 +94,18 @@ function makeIntlBubbles(container, radius="members", r_scale=20, r_div=12) {
     }
 
     let sim = d3.forceSimulation(data)
-        .force("charge", d3.forceManyBody().strength(2))
+        .force("charge", d3.forceManyBody().strength(1.5))
         .force("center", d3.forceCenter(w/2, h/2))
-        .force("x", d3.forceX().x(d => d.cx))
-        .force("y", d3.forceY().y(d => d.cy))
+        .force("x", d3.forceX().x(d => d.cx).strength(0.15))
+        .force("y", d3.forceY().y(d => d.cy).strength(50))
+        .force("y", d3.forceY().y(0))
         .force("collision", d3.forceCollide().radius(d => d.radius+1))
+        .alpha(0.15)
+        .alphaDecay(1 - Math.pow(0.001, 1 / 700)) 
         .on("tick", bubble_tick);
+    window.sim=sim;
 
-    let rescale = function(w) { };
-
+    //----------------TOOLTIP----------------------------------
     let tooltip = d3.select("body")
         .append("div")
         .attr("class", "tooltip")
@@ -112,6 +132,28 @@ function makeIntlBubbles(container, radius="members", r_scale=20, r_div=12) {
                 .style("left", box_x(event.pageX)+ "px");
         })
         .on("mouseout", tooltip.hide);
+    d3.select(window).on("scroll", tooltip.hide);
+
+    //----------------FUNCTIONS----------------------------------
+    let rescale = function(radius, r_scale=10, r_div=11) { 
+        radiusScale = d3.scalePow()
+            .exponent(0.5)
+            .domain(d3.extent(intls.map(d => d[radius])))
+            .range([4, r_scale + w/r_div]);
+        data = data.map(d => {
+            d.radius = radiusScale(d[radius]);
+            return d;
+        });
+
+        bubbles.attr("r", d => d.radius);
+        labels.text(d => d.radius > 10 ? d.abbr : "")
+            .attr("font-size", d => 3*d.radius / (d.abbr.length+2));
+
+        sim.force("collision", d3.forceCollide().radius(d => d.radius+1))
+            .alpha(0.2).restart();
+    };
+
+    return {rescale};
 }
 
 main()
