5d08aa2abd1a5ea44cebb0e9c3876eb8644ada4b
Cory McCartan <cory.mccartan@gmail.com>
2020-01-16 19:17:05 -0500

    rebuilding site Thu Jan 16 19:17:05 EST 2020

--- js/electorate-2020.js
+++ js/electorate-2020.js
@@ -2,10 +2,14 @@
 
 window.cap = function(s) { return s.charAt(0).toUpperCase().concat(s.slice(1)); };
 window.mod = (n, m) => ((n % m) + m) % m;
+window.f_pct = d3.format(".0%");
 
 async function load_data() {
     let electorate = await d3.csv("/data/electorate/sample.csv", d3.autoType);
-    //electorate = d3.shuffle(electorate);
+    window.dem_line = electorate.filter(d => d.lname == "DEM")[0]
+    window.gop_line = electorate.filter(d => d.lname == "GOP")[0]
+    electorate = electorate.filter(d => d.lname != "DEM" && d.lname != "GOP");
+    electorate = d3.shuffle(electorate);
     window.electorate = electorate;
 }
 
@@ -18,25 +22,57 @@ async function main() {
 
     await load_data();
     let N = electorate.length;
-    let ctr = -1;
-    window.d = electorate[0];
+    let ctr = 0;
+    let scrollTo = -1;
+    let cells = {};
 
     let next_profile = function() {
         ctr = mod(ctr + 1, N);
-        container.innerHTML = make_profile(electorate[ctr]);
-        d3.select("#next-profile").on("click", next_profile);
-        d3.select("#prev-profile").on("click", prev_profile);
+        scrollTo = cells._.findIndex(c => c.getBoundingClientRect().y >= innerHeight-20)-1;
+        setup_profile();
     };
     let prev_profile = function() {
         ctr = mod(ctr - 1, N);
-        container.innerHTML = make_profile(electorate[ctr]);
+        scrollTo = cells._.findIndex(c => c.getBoundingClientRect().y >= innerHeight-20)-1;
+        setup_profile();
+    };
+    let setup_profile = function() {
+        let voter = electorate[ctr];
+        window.d = voter;
+        container.innerHTML = make_profile(voter);
         d3.select("#next-profile").on("click", next_profile);
         d3.select("#prev-profile").on("click", prev_profile);
+
+        let questions = Object.keys(voter);
+        let row = d3.selectAll(".issues tr")
+            .data(questions, function(d) {
+                console.log(d);
+                return !!d ? d : this.id;
+            });
+        row.select("td#support")
+            .text(d => voter[d] == 1 ? "\u2713" : "")
+            .classed("agree", d => voter[d] == 1)
+            .classed("dem", d => dem_line[d] == 1)
+            .classed("gop", d => gop_line[d] == 1);
+        row.select("td#oppose")
+            .text(d => voter[d] == 2 ? "\u2713" : "")
+            .classed("agree", d => voter[d] == 2)
+            .classed("dem", d => dem_line[d] == 2)
+            .classed("gop", d => gop_line[d] == 2);
+
+        cells._ = Array.from(document.querySelectorAll("table.issues td"));
+        if (scrollTo >= 0) {
+            cells._[scrollTo].scrollIntoView(false);
+            window.scrollBy(0, 4);
+        }
     };
+    setup_profile();
 
-    next_profile();
-    container.style.visibility = "visible";
 
+    d3.select("body").on("keydown", () => {
+        if (d3.event.keyCode == 37) prev_profile();
+        if (d3.event.keyCode == 39) next_profile();
+    });
 }
 
 main();
